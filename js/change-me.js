faf69a416681901e42139ec3b9fcb37ead608cd0
hungtran <vav.feedforfun@gmail.com>
2019-12-25 14:14:43 +0700

    fix ui

--- js/change-me.js
+++ js/change-me.js
@@ -4,7 +4,7 @@ let manualGeneratorInterval = 1000;
 let priceTypeNames = ["Giải đặc biệt", "First price", "Second price", "Third price", "Common price"];
 let priceTypeColors = ["#ffb6c1", "#e7bd42", "#aaa9ad", "#cd7f32", "#20b2aa"];
 
-let bannerImage = "./di.png";
+let bannerImage = "./long-ass-dinner.png";
 
 let fontSize = 3;
 
