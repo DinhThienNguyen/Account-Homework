48fa2f041497fc7609a789cbfb6cb7a1a1385dc5
hungtran <vav.feedforfun@gmail.com>
2019-12-25 11:55:45 +0700

    fix banner

--- js/change-me.js
+++ js/change-me.js
@@ -4,7 +4,7 @@ let manualGeneratorInterval = 1000;
 let priceTypeNames = ["Giải đặc biệt", "First price", "Second price", "Third price", "Common price"];
 let priceTypeColors = ["#ffb6c1", "#e7bd42", "#aaa9ad", "#cd7f32", "#20b2aa"];
 
-let backgroundImage = "./2560x1440.jpg";
-let bannerImage = "./dinner.png";
+let backgroundImage = "./tet-bg-2.jpg";
+let bannerImage = "./long-ass-dinner.png";
 
 let audioFileToPlayWhileGenerating = new Audio("./NhacXoSo.mp3");
