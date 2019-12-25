f72748ccbc06db21d0b6e06b4782e8326775d1ec
ThienND97 <thiennd5751@gmail.com>
2019-12-25 14:37:32 +0700

    Update winner container grid

--- js/change-me.js
+++ js/change-me.js
@@ -1,11 +1,11 @@
 let generatorAutoInterval = 5000;
 let manualGeneratorInterval = 1000;
 
-let priceTypeNames = ["Giải đặc biệt", "First price", "Second price", "Third price", "Common price"];
+let priceTypeNames = ["Special price", "First price", "Second price", "Third price", "Common price"];
 let priceTypeColors = ["#ffb6c1", "#e7bd42", "#aaa9ad", "#cd7f32", "#20b2aa"];
 
-let bannerImage = "./di.png";
+let bannerImage = "./long-ass-dinner.png";
 
-let fontSize = 3;
+let fontSize = 2;
 
 let audioFileToPlayWhileGenerating = new Audio("./NhacXoSo.mp3");
